49318deb9524e600d9eb7a553bacfc28a021b367
kangkings <dlflq11@gmail.com>
2023-07-16 06:50:26 +0900

    Ch01.Ex1_3 Complete

---
+++ src/basic/algorithm/ch01_1/exercise/Ex1_3_judge.java
@@ -0,0 +1,15 @@
+package basic.algorithm.ch01_1.exercise;
+import java.util.Scanner;
+
+/*
+입력받은 숫자가 양수인지 음수인지 0인지 판별
+ */
+public class Ex1_3_judge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a<0) System.out.printf("%d은(는) 음수\n",a);
+        else if (a==0) System.out.printf("%d은(는) 0\n",a);
+        else System.out.printf("%d은(는) 양수",a);
+    }
+}
---
+++ src/basic/algorithm/ch01_1/exercise/Ex1c_1_median.java
@@ -0,0 +1,38 @@
+package basic.algorithm.ch01_1.exercise;
+import java.util.Scanner;
+
+/*
+입력받은 세 숫자 중 중앙값 구하기
+(이 과정은 나중에 퀵정렬에서 자세히 다룸)
+ */
+
+public class Ex1c_1_median {
+    static int med3(int a, int b, int c){
+        if(a >= b){
+            if(b>=c){
+                return b;
+            } else if (a < c) {
+                return a;
+            }else{
+                return c;
+            }
+        } else if (a > c) {
+            return a;
+        } else if (b>c) {
+            return c;
+        }else {
+            return b;
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("세 정수의 중앙값 구하기");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(med3(a,b,c));
+
+    }
+}
