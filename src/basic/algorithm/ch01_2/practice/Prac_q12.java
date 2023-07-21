e1276a8998b02f9e7f594f52c704f5ebf4792656
kangkings <dlflq11@gmail.com>
2023-07-22 02:43:55 +0900

    Ch01.Prac_q12 Complete

--- src/basic/algorithm/ch01_2/practice/Prac_q12.java
+++ src/basic/algorithm/ch01_2/practice/Prac_q12.java
@@ -3,10 +3,38 @@ package basic.algorithm.ch01_2.practice;
 import java.util.Scanner;
 
 public class Prac_q12 {
+    //왼쪽 아래가 직각인 직각이등변삼각형
     static void triangleLB(int n){
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }//왼쪽 위가 직각인 직각이등변삼각형
+    static void triangleLU(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j<(n-i)) System.out.printf("*");
+                else System.out.printf(" ");
+            }
+            System.out.println();
+        }
+    }//오른쪽 위가 직각인 직각이등변삼각형
+    static void triangleRU(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(j >= i) System.out.printf("*");
+                else System.out.printf(" ");
+            }
+            System.out.println();
+        }
+    }//오른쪽 아래가 직각인 직각이등변삼각형
+    static void triangleRB(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j < (n-i-1)) System.out.printf(" ");
+                else System.out.printf("*");
             }
             System.out.println();
         }
@@ -15,5 +43,11 @@ public class Prac_q12 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         triangleLB(n);
+        System.out.println();
+        triangleLU(n);
+        System.out.println();
+        triangleRB(n);
+        System.out.println();
+        triangleRU(n);
     }
 }
